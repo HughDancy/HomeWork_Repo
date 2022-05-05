7835eb8a66cd276d806a43b0e1cb87b1b6d70dcd
BorisKiselev <hugh.dancy@icloud.com>
2022-05-05 15:05:59 +0700

    delete comments
    delete all comments in xcode files

--- HomeWork_Repo/ViewController.swift
+++ HomeWork_Repo/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  HomeWork_Repo
-//
-//  Created by Борис Киселев on 05.05.2022.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
     }
 
     func setupView() {
